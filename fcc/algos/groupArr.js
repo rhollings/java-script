bdb97e4195eadbefddf06623b67c6d85008a0b64
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-07-01 11:46:14 +0200

    Create groupArr.js

---
+++ fcc/algos/groupArr.js
@@ -0,0 +1,34 @@
+/*
+Write a function that splits an array (first argument) into groups 
+the length of size (second argument) and returns them as a two-dimensional array.
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2) should return [["a", "b"], ["c", "d"]].
+
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3) should return [[0, 1, 2], [3, 4, 5]].
+*/
+
+function chunkArrayInGroups(arr, size) {
+  let temp = [];
+  let result = [];
+
+  for (let a = 0; a < arr.length; a++) {
+    if (a % size !== size - 1) temp.push(arr[a]);
+    else {
+      temp.push(arr[a]);
+      result.push(temp);
+      temp = [];
+    }
+  }
+
+  if (temp.length !== 0) result.push(temp);
+  return result;
+}
+
+/*
+Firstly, we create two empty arrays called temp and result, which we will eventually return.
+Our for loop loops until a is equal to or more than the length of the array in our test.
+Inside our loop, we push to temp using temp.push(arr[a]); if the remainder of a / size is not equal to size - 1.
+Otherwise, we push to temp, push temp to the result variable and reset temp to an empty array.
+Next, if temp isn’t an empty array, we push it to result.
+Finally, we return the value of result.
+*/
