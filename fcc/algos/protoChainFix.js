63f6991df2ef03a779d4c02aff9e3ea856a27316
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-07-07 21:09:08 +0200

    Update protoChainFix.js

--- fcc/algos/protoChainFix.js
+++ fcc/algos/protoChainFix.js
@@ -1,4 +1,12 @@
-
+/*
+The hasOwnProperty method is defined in Object.prototype,
+which can be accessed by Bird.prototype, which can then be accessed by duck. 
+This is an example of the prototype chain. In this prototype chain, 
+Bird is the supertype for duck, while duck is the subtype.
+Object is a supertype for both Bird and duck. 
+Object is a supertype for all objects in JavaScript. 
+Therefore, any object can use the hasOwnProperty method.
+*/
 
 
 
