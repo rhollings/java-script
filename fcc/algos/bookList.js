46680b605438aa0a5ba38d14ed71ab7355f6cd59
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-08-02 14:43:57 +0200

    Update bookList.js

--- fcc/algos/bookList.js
+++ fcc/algos/bookList.js
@@ -12,21 +12,20 @@ before the bookName parameter.
 var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
 
 // Change code below this line
-function add (bookName) {
-
-  bookList.push(bookName);
-  return bookList;
+function add (arr, bookName) {
+  let newArr = [...arr];
+  newArr.push(bookName);
+  return newArr;
   
   // Change code above this line
 }
 
 // Change code below this line
-function remove (bookName) {
-  var book_index = bookList.indexOf(bookName);
-  if (book_index >= 0) {
-
-    bookList.splice(book_index, 1);
-    return bookList;
+function remove (arr, bookName) {
+  let newArr = [...arr];
+  if (newArr.indexOf(bookName) >= 0) {
+    newArr.splice(newArr.indexOf(bookName), 1);
+    return newArr;
 
     // Change code above this line
     }
