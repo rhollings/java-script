703e071e9311feafed626bf9f32797024d469881
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-09-12 11:57:31 +0200

    Create dnaPair.js
    algo that matches the DNA pairs

---
+++ fcc/int_algos/dnaPair.js
@@ -0,0 +1,26 @@
+/*
+
+The DNA strand is missing the pairing element. Take each character, get its pair, and return the results as a 2d array.
+
+Base pairs are a pair of AT and CG. Match the missing element to the provided character.
+
+Return the provided character as the first element in each array.
+
+For example, for the input GCG, return [["G", "C"], ["C","G"], ["G", "C"]]
+
+The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
+
+*/
+
+var lookup = Object.create(null);
+lookup.A = 'T';
+lookup.T = 'A';
+lookup.C = 'G';
+lookup.G = 'C';
+
+function pairElement(str) {
+ return str.split('').map(function(p) {return [p, lookup[p]];});
+}
+
+pairElement("GCG");
+
