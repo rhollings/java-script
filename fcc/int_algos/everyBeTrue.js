cb561007d641ecb04e50909f0d1d7b2b0f9c3170
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-09-15 17:21:24 +0200

    Create everyBeTrue.js
    i believe, the algo wanted to return true if obj called wasn't empty but i am not 100% sure

---
+++ fcc/int_algos/everyBeTrue.js
@@ -0,0 +1,37 @@
+/*
+
+Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+
+In other words, you are given an array collection of objects. 
+The predicate pre will be an object property and you need to return true if its value is truthy. Otherwise, return false.
+
+In JavaScript, truthy values are values that translate to true when evaluated in a Boolean context.
+
+Remember, you can access object properties through either dot notation or [] notation.
+
+*/
+
+function truthCheck(collection, pre) {
+  // Create a counter to check how many are true.
+  var counter = 0;
+  // Check for each object
+  for (var c in collection) {
+    // If it is has property and value is truthy
+    if (collection[c].hasOwnProperty(pre) && Boolean(collection[c][pre])) {
+      counter++;
+    }
+  }
+  // Outside the loop, check to see if we got true for all of them and return true or false
+  return counter == collection.length;
+}
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+
+/*
+
+First I create a counter to check how many cases are actually true.
+Then check for each object if the value is truthy
+Outside the loop, I check to see if the counter variable has the same value as the length of collection, 
+if true then return true, otherwise, return false
+
+*/
