7ffa72b9468f269c47b7def34178b4f7752f22b3
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-09-16 17:20:57 +0200

    Create argOpt.js
    Create a function that sums two arguments together

---
+++ fcc/int_algos/argOpt.js
@@ -0,0 +1,62 @@
+/*
+
+Create a function that sums two arguments together. If only one argument is provided,
+then return a function that expects one argument and returns the sum.
+
+For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.
+
+Calling this returned function with a single argument will then return the sum:
+
+var sumTwoAnd = addTogether(2);
+sumTwoAnd(3) returns 5.
+
+If either argument isn't a valid number, return undefined.
+
+*/
+
+function addTogether() {
+  const [first, second] = arguments;
+  // First argument is not a number
+  if (typeof(first) !== "number") {
+    return undefined;
+  }
+  // First argument is a number
+  //  and second argument is not defined
+  else if (second === undefined) {
+    function addSecond(second) {
+      // New argument is not a number
+      if (typeof(second) !== "number") {
+        return undefined;
+      }
+      // New argument is a number
+      else {
+        return first + second;
+      }
+    }
+    // Note: returning a *function*
+    return addSecond;
+  }
+  // First argument is a number
+  //  and second argument is not a number
+  else if (typeof(second) !== "number") {
+    return undefined;
+  }
+  // First argument is a number
+  //  and second argument is a number
+  else {
+    return first + second;
+  }
+}
+
+/*
+
+Hint 1
+Every time you deal with an argument, you have to check if it is defined and if it is a number.
+
+Hint 2
+When working on the case that it needs to return a function, using closure can help you write the new function in terms of addTogether().
+
+Hint 3
+The early return pattern can simplify your code.
+
+*/
