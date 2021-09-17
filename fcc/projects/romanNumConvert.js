2fdaeb2f09c1434d86e02500da7a9cda4f7423dd
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-09-17 11:24:08 +0200

    Create romanNumConvert.js
    convert num into roman numeral

---
+++ fcc/projects/romanNumConvert.js
@@ -0,0 +1,28 @@
+/*
+
+Convert the given number into a roman numeral.
+
+All roman numerals answers should be provided in upper-case.
+
+*/
+
+
+function convertToRoman(num) {
+  var ref = [['M', 1000], ['CM', 900], ['D', 500], ['CD', 400], ['C', 100], ['XC', 90], ['L', 50], ['XL', 40], ['X', 10], ['IX', 9], ['V', 5], ['IV', 4], ['I', 1]];
+  var res = [];
+  ref.forEach(function(p) {
+    while (num >= p[1]) {
+      res.push(p[0]);
+      num -= p[1];
+    }
+  });
+  return res.join('');
+}
+
+/*
+
+create list of all roman numeral options
+iterate of each num and give its value respectively
+use forEach to list each item in list in order
+
+*/
