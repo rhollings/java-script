daba9e6f55dadfd7e5d52777e9abb264095baafa
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-09-29 17:53:51 +0200

    Create counter.js

---
+++ scripts/counter.js
@@ -0,0 +1,43 @@
+/*
+The Counter component keeps track of a count value in state. 
+There are two buttons which call methods increment() and decrement().
+Write these methods so the counter value is incremented or decremented by 1 when the appropriate button is clicked.
+Also, create a reset() method so when the reset button is clicked, the count is set to 0.
+*/
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+  increment() {
+    this.setState(state => ({
+      count: state.count + 1
+    }));
+  }
+  decrement() {
+    this.setState(state => ({
+      count: state.count - 1
+    }));
+  }
+  reset() {
+    this.setState({
+      count: 0
+    });
+  }
+  render() {
+    return (
+      <div>
+        <button className='inc' onClick={this.increment}>Increment!</button>
+        <button className='dec' onClick={this.decrement}>Decrement!</button>
+        <button className='reset' onClick={this.reset}>Reset</button>
+        <h1>Current Count: {this.state.count}</h1>
+      </div>
+    );
+  }
+};
