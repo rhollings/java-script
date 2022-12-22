8609119a8079d5f17f890813750e09cb3e336ff0
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2022-12-21 17:48:49 -0700

    Update basicJS.js

--- basics/basicJS.js
+++ basics/basicJS.js
@@ -6,3 +6,7 @@ let y;
 x = 5;
 y = 6;
 let z = x + y;
+
+// Use a regular expression to do a case-insensitive search for "w3schools" in a string:
+let text = "Visit W3Schools";
+let n = text.search(/w3schools/i); 
